1212bbf59f74926b3c872394da619765fa816ff2
Dennis Dörr <dev@ddoerr.com>
2020-01-20 17:18:44 +0100

    added meta events to the EventBus

--- src/main/java/com/ddoerr/scriptit/EventBus.java
+++ src/main/java/com/ddoerr/scriptit/EventBus.java
@@ -5,34 +5,43 @@ import com.ddoerr.scriptit.api.libraries.NamespaceRegistry;
 import java.util.*;
 import java.util.function.Consumer;
 
-public class EventBus implements Bus<NamespaceRegistry> {
-    Map<String, List<Consumer<NamespaceRegistry>>> subscribers = new HashMap<>();
+public class EventBus implements Bus<Object> {
+    Map<String, List<Consumer<Object>>> subscribers = new HashMap<>();
 
     @Override
-    public void subscribe(String id, Consumer<NamespaceRegistry> consumer) {
+    public void subscribe(String id, Consumer<Object> consumer) {
         if (subscribers.containsKey(id)) {
             subscribers.get(id).add(consumer);
         } else {
             subscribers.put(id, new ArrayList<>(Collections.singletonList(consumer)));
+            publish("bus:added", id);
         }
+
+        publish("bus:subscribed", id);
     }
 
     @Override
-    public void unsubscribe(String id, Consumer<NamespaceRegistry> consumer) {
+    public void unsubscribe(String id, Consumer<Object> consumer) {
         if (!subscribers.containsKey(id)) {
             return;
         }
 
         subscribers.get(id).remove(consumer);
+        publish("bus:unsubscribed", id);
+
+        if (subscribers.get(id).isEmpty()) {
+            subscribers.remove(id);
+            publish("bus:removed", id);
+        }
     }
 
     @Override
-    public void publish(String id, NamespaceRegistry data) {
+    public void publish(String id, Object data) {
         if (!subscribers.containsKey(id)) {
             return;
         }
 
-        for (Consumer<NamespaceRegistry> consumer : subscribers.get(id)) {
+        for (Consumer<Object> consumer : subscribers.get(id)) {
             consumer.accept(data);
         }
     }
