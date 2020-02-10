be84c3d0d5f539812cbfce68c9ffbe2f1dea6c2d
Dennis Dörr <dev@ddoerr.com>
2020-02-10 20:40:13 +0100

    fixed KeyBindingButtonWidget

--- src/main/java/com/ddoerr/scriptit/widgets/KeyBindingButtonWidget.java
+++ src/main/java/com/ddoerr/scriptit/widgets/KeyBindingButtonWidget.java
@@ -21,23 +21,6 @@ public class KeyBindingButtonWidget extends WButton {
 
     private Consumer<InputUtil.KeyCode> onChange;
 
-    public KeyBindingButtonWidget(WPosition position, WSize size, WInterface linkedInterface) {
-        super(position, size, linkedInterface);
-
-        setLabel(new LiteralText(getFormattedKey()));
-    }
-
-    public KeyBindingButtonWidget(WPosition position, WSize size, WInterface linkedInterface, InputUtil.KeyCode keyCode) {
-        super(position, size, linkedInterface);
-
-        this.keyCode = keyCode;
-        setLabel(new LiteralText(getFormattedKey()));
-    }
-
-    public static Theme of(Map<String, String> rawTheme) {
-        return WButton.of(rawTheme);
-    }
-
     public void setKeyCode(InputUtil.KeyCode keyCode) {
         this.keyCode = keyCode;
         setLabel(new LiteralText(getFormattedKey()));
@@ -81,27 +64,6 @@ public class KeyBindingButtonWidget extends WButton {
             if (onChange != null) {
                 onChange.accept(keyCode);
             }
-        }
-    }
-
-    // Copied and modified from WDropdown::draw
-    @Override
-    public void draw() {
-        if (!this.isHidden()) {
-            if (this.isLowered()) {
-                BaseRenderer.drawBeveledPanel((double) this.getX(), (double) this.getY(), (double) this.getZ(), (double) this.getWidth(), (double) this.getHeight(), this.getResourceAsColor(0), this.getResourceAsColor(2), this.getResourceAsColor(1));
-            } else {
-                BaseRenderer.drawBeveledPanel((double) this.getX(), (double) this.getY(), (double) this.getZ(), (double) this.getWidth(), (double) this.getHeight(), this.getResourceAsColor(3), this.getResourceAsColor(5), this.getResourceAsColor(4));
-            }
-
-            if (this.hasLabel()) {
-                if (BaseRenderer.getTextRenderer().getStringWidth(this.getLabel().asFormattedString()) > this.getWidth() - 6) {
-                    BaseRenderer.drawText(this.isLabelShadowed(), this.getLabel().asFormattedString(), this.getX() + this.getWidth() + 2, (int) ((double) (this.getY() + this.getHeight() / 2) - 4.5D), this.getResourceAsColor(6).RGB);
-                } else {
-                    BaseRenderer.drawText(this.isLabelShadowed(), this.getLabel().asFormattedString(), this.getX() + this.getWidth() / 2 - BaseRenderer.getTextRenderer().getStringWidth(this.getLabel().asFormattedString()) / 2, this.getY() + 6, this.getResourceAsColor(6).RGB);
-                }
-            }
-
         }
     }
 
