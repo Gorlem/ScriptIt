37d20d92b564c1501acbc4a5c7c35d2550a2b5df
Dennis Dörr <dev@ddoerr.com>
2020-02-29 15:52:46 +0100

    Added mixin to access fps

---
+++ src/main/java/com/ddoerr/scriptit/mixin/FpsAccessor.java
@@ -0,0 +1,11 @@
+package com.ddoerr.scriptit.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin({MinecraftClient.class})
+public interface FpsAccessor {
+    @Accessor
+    static int getCurrentFps() { return 0; }
+}
