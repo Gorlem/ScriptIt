c5584d3d27f5d515df9a199f62e2e77c4fe5fc1e
Dennis Dörr <dev@ddoerr.com>
2020-03-06 14:59:28 +0100

    Abstracted away common logic from Spinnery Button/Slot provider into AbstractSpinneryClickablesProvider

--- src/main/java/com/ddoerr/scriptit/util/buttons/SpinneryButtonProvider.java
+++ src/main/java/com/ddoerr/scriptit/util/buttons/SpinneryButtonProvider.java
@@ -1,18 +1,20 @@
 package com.ddoerr.scriptit.util.buttons;
 
+import com.ddoerr.scriptit.util.AbstractSpinneryClickablesProvider;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.AbstractPressableButtonWidget;
 import spinnery.client.BaseScreen;
 import spinnery.widget.WAbstractButton;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-public class SpinneryButtonProvider implements ButtonProvider {
+public class SpinneryButtonProvider extends AbstractSpinneryClickablesProvider<WAbstractButton> implements ButtonProvider {
+    public SpinneryButtonProvider() {
+        super(WAbstractButton.class);
+    }
+
     @Override
     public void click(Screen screen, int index) {
-        List<WAbstractButton> buttons = getButtons(screen);
+        List<WAbstractButton> buttons = getWidgets((BaseScreen)screen);
 
         if (index < 0 || index >= buttons.size()) {
             return;
@@ -20,36 +22,5 @@ public class SpinneryButtonProvider implements ButtonProvider {
 
         WAbstractButton button = buttons.get(index);
         button.onMouseClicked(button.getX() + 1, button.getY() + 1, 0);
-    }
-
-    @Override
-    public int getAmount(Screen screen) {
-        return getButtons(screen).size();
-    }
-
-    @Override
-    public boolean matches(Screen screen) {
-        return screen instanceof BaseScreen;
-    }
-
-    @Override
-    public void renderTooltip(Screen screen, int mouseX, int mouseY) {
-        List<WAbstractButton> buttons = getButtons(screen);
-
-        for (int i = 0; i < buttons.size(); i++) {
-            WAbstractButton button = buttons.get(i);
-
-            if (button.isWithinBounds(mouseX, mouseY)) {
-                screen.renderTooltip("Button " + i, mouseX, mouseY);
-            }
-        }
-    }
-
-    private List<WAbstractButton> getButtons(Screen screen) {
-        return ((BaseScreen)screen).getInterface().getAllWidgets()
-                .stream()
-                .filter(WAbstractButton.class::isInstance)
-                .map(WAbstractButton.class::cast)
-                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/ddoerr/scriptit/util/slots/SpinnerySlotProvider.java
+++ src/main/java/com/ddoerr/scriptit/util/slots/SpinnerySlotProvider.java
@@ -1,18 +1,21 @@
 package com.ddoerr.scriptit.util.slots;
 
+import com.ddoerr.scriptit.util.AbstractSpinneryClickablesProvider;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.container.SlotActionType;
 import spinnery.client.BaseScreen;
-import spinnery.widget.WAbstractButton;
 import spinnery.widget.WSlot;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class SpinnerySlotProvider implements SlotProvider {
+public class SpinnerySlotProvider extends AbstractSpinneryClickablesProvider<WSlot> implements SlotProvider {
+    public SpinnerySlotProvider() {
+        super(WSlot.class);
+    }
+
     @Override
     public void click(Screen screen, int index, int button, SlotActionType actionType) {
-        List<WSlot> slots = getSlots(screen);
+        List<WSlot> slots = getWidgets((BaseScreen)screen);
 
         if (index < 0 || index >= slots.size()) {
             return;
@@ -21,36 +24,5 @@ public class SpinnerySlotProvider implements SlotProvider {
         WSlot slot = slots.get(index);
         // TODO: Support the different SlotActionTypes
         slot.onMouseClicked(slot.getX() + 1, slot.getY() + 1, button);
-    }
-
-    @Override
-    public int getAmount(Screen screen) {
-        return getSlots(screen).size();
-    }
-
-    @Override
-    public boolean matches(Screen screen) {
-        return screen instanceof BaseScreen;
-    }
-
-    @Override
-    public void renderTooltip(Screen screen, int mouseX, int mouseY) {
-        List<WSlot> slots = getSlots(screen);
-
-        for (int i = 0; i < slots.size(); i++) {
-            WSlot slot = slots.get(i);
-
-            if (slot.isWithinBounds(mouseX, mouseY)) {
-                screen.renderTooltip("Slot " + i, mouseX, mouseY);
-            }
-        }
-    }
-
-    private List<WSlot> getSlots(Screen screen) {
-        return ((BaseScreen)screen).getInterface().getAllWidgets()
-                .stream()
-                .filter(WSlot.class::isInstance)
-                .map(WSlot.class::cast)
-                .collect(Collectors.toList());
     }
 }
