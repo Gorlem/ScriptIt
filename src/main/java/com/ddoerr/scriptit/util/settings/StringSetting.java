6924613885a6f6a29d353ad70ecbc35460c50c47
Dennis Dörr <dev@ddoerr.com>
2020-03-07 08:12:18 +0100

    Fixed StringSetting

--- src/main/java/com/ddoerr/scriptit/util/settings/StringSetting.java
+++ src/main/java/com/ddoerr/scriptit/util/settings/StringSetting.java
@@ -5,8 +5,8 @@ import com.ddoerr.scriptit.util.ObjectConverter;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class StringSetting extends AbstractSetting {
-    public StringSetting(String name, Supplier getter, Consumer setter) {
+public class StringSetting extends AbstractSetting<String> {
+    public StringSetting(String name, Supplier<String> getter, Consumer<String> setter) {
         super(name, getter, setter);
     }
 
