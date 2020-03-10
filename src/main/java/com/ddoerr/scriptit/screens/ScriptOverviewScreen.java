12a417d40e54f5fafa0cb2e622a9e741727a6c7c
Dennis Dörr <dev@ddoerr.com>
2020-03-10 17:53:16 +0100

    Made ScriptOverviewScreen translatable

--- src/main/java/com/ddoerr/scriptit/screens/ScriptOverviewScreen.java
+++ src/main/java/com/ddoerr/scriptit/screens/ScriptOverviewScreen.java
@@ -1,5 +1,6 @@
 package com.ddoerr.scriptit.screens;
 
+import com.ddoerr.scriptit.ScriptItMod;
 import com.ddoerr.scriptit.callbacks.ConfigCallback;
 import com.ddoerr.scriptit.dependencies.Resolver;
 import com.ddoerr.scriptit.scripts.ScriptContainer;
@@ -7,6 +8,8 @@ import com.ddoerr.scriptit.scripts.Scripts;
 import com.ddoerr.scriptit.triggers.Trigger;
 import com.ddoerr.scriptit.widgets.PanelWidget;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
 import spinnery.widget.*;
 import spinnery.widget.api.Position;
 import spinnery.widget.api.Size;
@@ -65,13 +68,13 @@ public class ScriptOverviewScreen extends AbstractHistoryScreen {
 
         row.createChild(WButton.class)
                 .setSize(Size.of(45, 20))
-                .setLabel("Edit")
+                .setLabel(new TranslatableText(new Identifier(ScriptItMod.MOD_NAME, "scripts.edit").toString()))
                 .setOnMouseClicked((widget, mouseX, mouseY, delta) -> history.open(() -> new ScriptEditorScreen(scriptContainer)))
                 .setOnAlign(w -> w.setPosition(Position.ofTopRight(row).add(-110, 0, 0)));
 
         row.createChild(WButton.class)
                 .setSize(Size.of(45, 20))
-                .setLabel("Remove")
+                .setLabel(new TranslatableText(new Identifier(ScriptItMod.MOD_NAME, "scripts.remove").toString()))
                 .setOnMouseClicked((widget, mouseX, mouseY, delta) -> {
                     Resolver.getInstance().resolve(Scripts.class).remove(scriptContainer);
                     Trigger trigger = scriptContainer.getTrigger();
@@ -93,7 +96,7 @@ public class ScriptOverviewScreen extends AbstractHistoryScreen {
         panel.createChild(WButton.class)
                 .setSize(Size.of(100, 20))
                 .setOnAlign(w -> w.setPosition(Position.of(panel, 4, 4)))
-                .setLabel("Add new Script")
+                .setLabel(new TranslatableText(new Identifier(ScriptItMod.MOD_NAME, "scripts.add").toString()))
                 .setOnMouseClicked((widget, mouseX, mouseY, delta) -> history.open(ScriptEditorScreen::new));
     }
 
@@ -105,7 +108,7 @@ public class ScriptOverviewScreen extends AbstractHistoryScreen {
         panel.createChild(WButton.class)
                 .setSize(Size.of(100, 20))
                 .setOnAlign(w -> w.setPosition(Position.of(panel, 4, 4)))
-                .setLabel("Open Designer")
+                .setLabel(new TranslatableText(new Identifier(ScriptItMod.MOD_NAME, "scripts.edit").toString()))
                 .setOnMouseClicked((widget, mouseX, mouseY, delta) -> history.open(HudElementOverviewScreen::new));
     }
 }
