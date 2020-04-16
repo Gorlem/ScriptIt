23934d6be912ee93aeb163b184e6cd480de802c2
Dennis Dörr <dev@ddoerr.com>
2020-04-16 22:22:04 +0200

    Updated dependencies and increased to version v0.4 (#44)

--- src/main/java/com/ddoerr/scriptit/mixin/PacketTextAccessor.java
+++ src/main/java/com/ddoerr/scriptit/mixin/PacketTextAccessor.java
@@ -1,6 +1,6 @@
 package com.ddoerr.scriptit.mixin;
 
-import net.minecraft.client.network.packet.ChatMessageS2CPacket;
+import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
