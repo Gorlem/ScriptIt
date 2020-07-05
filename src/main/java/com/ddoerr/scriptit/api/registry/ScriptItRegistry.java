b92652684e3258a378df5114f096234b862209a7
Dennis Dörr <dev@ddoerr.com>
2020-07-05 13:42:48 +0200

    ScriptItRegistry is now itself a registry

--- src/main/java/com/ddoerr/scriptit/api/registry/ScriptItRegistry.java
+++ src/main/java/com/ddoerr/scriptit/api/registry/ScriptItRegistry.java
@@ -5,12 +5,21 @@ import com.ddoerr.scriptit.api.events.Event;
 import com.ddoerr.scriptit.api.hud.HudElement;
 import com.ddoerr.scriptit.api.languages.Language;
 import com.ddoerr.scriptit.api.libraries.Model;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.DefaultedRegistry;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.SimpleRegistry;
 
-public class ScriptItRegistry {
+public class ScriptItRegistry extends SimpleRegistry<Registry<?>> {
     public SimpleRegistry<Model> libraries = new SimpleRegistry<>();
     public DefaultedRegistry<Language> languages = new DefaultedRegistry<>(ScriptItMod.MOD_NAME + ":lua");
     public SimpleRegistry<Event> events = new SimpleRegistry<>();
     public SimpleRegistry<HudElement> hudElements = new SimpleRegistry<>();
+
+    public ScriptItRegistry() {
+        add(new Identifier(ScriptItMod.MOD_NAME, "libraries"), libraries);
+        add(new Identifier(ScriptItMod.MOD_NAME, "languages"), languages);
+        add(new Identifier(ScriptItMod.MOD_NAME, "events"), events);
+        add(new Identifier(ScriptItMod.MOD_NAME, "hudElements"), hudElements);
+    }
 }
